19729a31fd27aa76bf8d7e9e1842e60223ca6e74
Paulina <paulinapedroza848@gmail.com>
2025-05-21 15:28:34 -0600

    hey

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/temperatura", (req, res) => {
+  res.json({ valor: "10 °C", timestamp: new Date().toISOString() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
